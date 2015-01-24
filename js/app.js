2a820025d396beefe142598015277f6f5d0990fe
Nadeesha Cabral <nadeesha.cabral@gmail.com>
2015-01-25 03:18:52 +0530

    termish install and run

--- js/app.js
+++ js/app.js
@@ -3,8 +3,10 @@
     'use strict';
 
     termish.db = new PouchDB('termish');
+    termish.input = $('#input');
+    termish.output = $('#output');
 
-    termish.installScript = function(name, gistUrl) {
+    termish.installScript = function(name, gistUrl, cb) {
         $.get(gistUrl, saveToDb);
 
         function saveToDb(fn) {
@@ -13,11 +15,7 @@
                 name: name,
                 fn: fn
             }, function(err) {
-                if (!err) {
-                    console.log('script saved');
-                } else {
-                    console.log(err);
-                }
+                cb(err);
             });
         }
     };
@@ -39,10 +37,39 @@
             } else if (response.rows.length === 0) {
                 console.log('no scripts found by that name');
             } else {
-                var fn = new Function(response.rows[0].key); // ugh!
-                fn.bind(null, args);
+                var Fn = eval('(' + response.rows[0].key + ')'); // ugh!
+                var fn = new Fn(args);
+                fn.exec(termish.sendOutput);
             }
         });
+    };
+
+    termish.input.on('keydown', function(event) {
+        if (event.which == 13) {
+            termish.runCmd($('#input').val());
+        }
+    });
+
+    termish.runCmd = function(cmd) {
+        cmd = cmd.split(' ');
+
+        if (cmd[0] === 'termish') {
+            termish.commands[cmd[1]](cmd.slice(2), termish.sendOutput);
+        } else {
+            termish.executeScript(cmd[0], cmd.slice(1));
+        }
+    };
+
+    termish.sendOutput = function(err, data) {
+        termish.output.html(err || data);
+    };
+
+    termish.commands = {
+        install: function(args, cb) {
+            termish.installScript(args[0], args[1], function(err) {
+                cb(err || 'script saved');
+            });
+        }
     };
 
 })(window.termish = window.termish || {}, PouchDB, console, jQuery);
