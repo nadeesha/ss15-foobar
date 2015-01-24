61a95dbda9503fb93f8c68e5fa69885fc6297dbe
Pasindu De Silva <ppasindud@gmail.com>
2015-01-24 21:25:13 +0530

    added js sandbox and some testscripts

--- js/app.js
+++ js/app.js
@@ -1,3 +1,6 @@
+var gobalTestString = "TestSandbox Gobal Var";
+
+
 (function(termish, PouchDB, console) {
 
     'use strict';
@@ -14,6 +17,8 @@
             fn: fn
         }, function(err) {
             if (!err) {
+                // Replication
+                // PouchDB.replicate('termish', 'http://localhost:5984/termish', {live: true});
                 console.log('script saved');
             } else {
                 console.log(err);
@@ -22,6 +27,7 @@
     };
 
     termish.executeScript = function(name, args) {
+        args = args || "";
         function findLatestScript(doc, emit) {
             if (doc.type === 'script' && doc.name === name) {
                 emit(doc.fn);
@@ -33,15 +39,30 @@
         }, {
             include_docs: true
         }, function(err, response) {
+
             if (err) {
                 console.log(err);
             } else if (response.rows.length === 0) {
                 console.log('no scripts found by that name');
             } else {
-                var fn = new Function(response.rows[0].key); // ugh!
-                fn.bind(null, args);
+                var strFn = response.rows[0].key;
+                // var fn = new Function(response.rows[0].key); // ugh!
+                // fn.bind(null, args);
+
+                var sandbox   = new JSandbox();
+                console.log(strFn + ";"+name+"();");
+                sandbox.eval(strFn + ";"+name+"(input);",
+                  function(r) { // Callback
+                    console.log(r);
+                  },
+                  args,
+                  function  (err) { // Onerror
+                    console.log(err);
+                });
             }
         });
     };
 
 })(window.termish = window.termish || {}, PouchDB, console);
+
+
---
+++ js/set_sample.js
@@ -0,0 +1,20 @@
+
+var cal_sum = 'function cal_sum (line){var numbers = line.split(" ");return numbers[0] + numbers[1];}';
+var cal_sub = 'function cal_sub (line){var numbers = line.split(" ");return numbers[0] - numbers[1];}';
+var cal_multi = 'function cal_multi (line){var numbers = line.split(" ");return numbers[0] * numbers[1];}';
+
+
+var testGobal = 'function testGobal (line){ console.log(gobalTestString) }';
+
+termish.installScript("cal_sum",1,cal_sum);
+termish.installScript("cal_sub",1,cal_sub);
+termish.installScript("cal_multi",1,cal_multi);
+termish.installScript("testGobal",1,testGobal);
+
+
+termish.executeScript("cal_sum","1 4");
+termish.executeScript("cal_sub","1 4");
+termish.executeScript("cal_multi","1 4");
+
+termish.executeScript("testGobal","1 4");
+
